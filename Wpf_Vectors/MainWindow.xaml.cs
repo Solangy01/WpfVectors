f292486c60cb5b064ffb324b095cff17f8211de5
Валерия Дактан <daktanlera@gmail.com>
2023-05-21 01:06:15 +0700

    Добавьте файлы проекта.

---
+++ Wpf_Vectors/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_Vectors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Vector3D vector1;
+        Vector3D vector2;
+        Vector3D vector3;
+
+
+        // Генерация первых двух векторов - первые два вектора везде используются
+        void GenerateVectors()
+        {
+            vector1 = Vector3D.Parse(Tb_Vec1.Text);
+            vector2 = Vector3D.Parse(Tb_Vec2.Text);
+        }
+
+
+        // Обработчики событий нажатия на кнопки
+
+
+        private void Btn_Sum_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            Tb_Res.Text = (vector1 + vector2).ToString();
+        }
+
+        private void Btn_Dif_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            Tb_Res.Text = (vector1 - vector2).ToString();
+        }
+
+        private void Btn_Scalar_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            Tb_Res.Text = (vector1 & vector2).ToString();
+        }
+
+        private void Btn_Vec_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            Tb_Res.Text = (vector1 * vector2).ToString();
+        }
+
+        private void Btn_Mix_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            vector3 = Vector3D.Parse(Tb_Vec3.Text);
+            Tb_Res.Text = vector1.MixProd(vector2, vector3).ToString();
+        }
+
+        private void Btn_Angle_Click(object sender, RoutedEventArgs e)
+        {
+            GenerateVectors();
+            Tb_Res.Text = vector1.Angle(vector2).ToString();
+        }
+    }
+}
---
+++ Wpf_Vectors/Vector3D.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace Wpf_Vectors
+{
+    public class Vector3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string? ToString()
+        {
+            return $"{X}; {Y}; {Z}";
+        }
+
+        public static Vector3D Parse(string text)
+        {
+            double[] coords = text.Split(' ', StringSplitOptions.RemoveEmptyEntries) // разделяем строку на массив
+                                    .Select(coord => double.Parse(coord)).ToArray(); // и преобразуем его в массив double 
+
+            return new Vector3D(coords[0], coords[1], coords[2]);
+        }
+
+
+        // Переопределение оператора +
+        // Суммирование
+        public static Vector3D operator +(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        // Разность
+        public static Vector3D operator -(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        // Вектороное произведение
+        public static Vector3D operator *(Vector3D a, Vector3D b)
+        {
+            return new Vector3D(a.Y*b.Z - a.Z*b.Y,
+                                a.Z*b.X - a.X*b.Z,
+                                a.X*b.Y - a.Y*b.X);
+        }
+
+        // Скалярное произведение
+        public static double operator &(Vector3D a, Vector3D b)
+        {
+            return a.X*b.X + a.Y*b.Y + a.Z*b.Z;
+        }
+
+        // Смешанное произведение
+        public double MixProd(Vector3D b, Vector3D c)
+        {
+            return this & b * c;
+        }
+
+        // Вычисление длины
+        public double Length()
+        {
+            return Math.Sqrt(X*X + Y*Y + Z*Z);
+        }
+
+        // Вычисление угла между векторами
+        public double Angle(Vector3D vector)
+        {
+            return (this & vector) / (this.Length() * vector.Length());
+        }
+    }
+}
